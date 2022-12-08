2feb00364f11f63536a5eb9cc15e269f56b3932d
arsen <dabblerfar@yandex.ru>
2022-12-08 18:52:44 +0300

    M

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -165,7 +165,8 @@ fun times(a: List<Int>, b: List<Int>): Int {
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0 при любом x.
  */
-fun polynom(p: List<Int>, x: Int): Int = p.sumOf { it * x.toDouble().pow(p.indexOf(it)).toInt() }
+fun polynom(p: List<Int>, x: Int): Int =
+    p.withIndex().sumOf { (index, value) -> value * x.toDouble().pow(index).toInt() }
 
 /**
  * Средняя (3 балла)
